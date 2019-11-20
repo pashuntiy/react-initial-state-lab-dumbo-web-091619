8173561d533319954cf72eca3ae8894066f84416
github username <github email address>
2019-11-20 11:10:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Bomb from './Bomb';
 ReactDOM.render(
   <div>
     <ImageSlider />
-    <Bomb initialCount={120} />
+    <Bomb initialCount={10}/>
   </div>,
   document.getElementById('root')
 );
